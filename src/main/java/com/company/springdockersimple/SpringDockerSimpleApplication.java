8a3da574371da70e2fc3c51f2519c200158d09ed
Илья <qq.ferveks@gmail.com>
2022-04-24 12:34:18 +0300

    Initial commit

---
+++ src/main/java/com/company/springdockersimple/SpringDockerSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.company.springdockersimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerSimpleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDockerSimpleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/company/springdockersimple/SpringDockerSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.springdockersimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerSimpleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
